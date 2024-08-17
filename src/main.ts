6efc6111b3f704b52d1f56f22287e470ac51faec
kdj309 <karthik.joshi103@gmail.com>
2024-08-17 21:37:38 +0530

    enabled cookie

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { config } from './config/config';
 require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:"http://localhost:5173",
+    credentials:true
+  })
   await app.listen(config().port);
 }
 bootstrap();
