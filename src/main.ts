578feb55dd72300d1179fb82c9d20ad489fb6554
kdj309 <karthik.joshi103@gmail.com>
2026-08-29 23:17:44 +0530

    added trust proxy

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.getHttpAdapter().getInstance().set('trust proxy', 1);
   app.use(cookieParser());
   app.enableCors({
     origin: process.env.FRONTEND_ORIGIN
